af4cd7fc76feed0b1b804d67c3602c096d3bd66b
Gary Moore <garyprm@outlook.com>
2020-06-07 00:21:58 +0100

    Add rosyln analysers and remove dotnet framework project

--- SecretsCoreApp.Web/Infrastructure/DatabaseConfig.cs
+++ SecretsCoreApp.Web/Infrastructure/DatabaseConfig.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SecretsCoreApp.Web.Infrastructure
+﻿namespace SecretsCoreApp.Web.Infrastructure
 {
     public class DatabaseConfig
     {
--- SecretsCoreApp.Web/Models/IndexViewModel.cs
+++ SecretsCoreApp.Web/Models/IndexViewModel.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SecretsCoreApp.Web.Models
+﻿namespace SecretsCoreApp.Web.Models
 {
     public class IndexViewModel
     {
         public string ApiKey { get; set; }
-        public string ConnectionString { get; set; }    
+        public string ConnectionString { get; set; }
     }
 }
